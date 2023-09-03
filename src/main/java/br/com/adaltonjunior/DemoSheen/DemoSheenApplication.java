4f1b32350bf879eb0274f20349d463295f977401
AdaltonJunior <adaltonsnjunior@gmail.com>
2023-09-03 19:45:46 +0100

    Initial Commit

---
+++ src/main/java/br/com/adaltonjunior/DemoSheen/DemoSheenApplication.java
@@ -0,0 +1,13 @@
+package br.com.adaltonjunior.DemoSheen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSheenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSheenApplication.class, args);
+	}
+
+}
